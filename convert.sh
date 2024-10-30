690a1338b753b6143752ed68c4a184275bd64ba4
Jakob Voss <voss@gbv.de>
2024-10-30 11:07:08 +0100

    Simplify output of convert

--- convert.sh
+++ convert.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 
 [ -d lido2rdf ] || git clone https://github.com/nfdi4objects/lido-rdf-converter.git
-(cd lido2rdf && git pull && make deps)
+(cd lido2rdf && git pull && make deps) >/dev/null
 
 for xml in *.xml; do
-   ttl=${xml/.xml/.ttl}
-   echo $xml
-   ./lido2rdf/.venv/bin/python ./lido2rdf/lido2rdf.py -m ./lido2rdf/lido2rdf.x3ml $xml > $ttl
+    tmp=${xml/.xml/.tmp}
+    ttl=${xml/.xml/.ttl}
+    echo -n $xml
+    ./lido2rdf/.venv/bin/python ./lido2rdf/lido2rdf.py -m ./lido2rdf/lido2rdf.x3ml $xml 2>/dev/null > $tmp
+    if [ $? -eq 0 ]; then
+       mv $tmp $ttl
+       echo " OK"
+    else
+       echo " FAIL"
+       rm $tmp
+    fi
 done
